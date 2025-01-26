02c30830ff060831255d25693f676582eff70774
anttonalberdi <anttonalberdi@gmail.com>
2025-01-26 06:09:10 +0100

    Fix dynamic inputs

---
+++ workflow/scripts/concatenate_clusters.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import pandas as pd
+
+def main(input_files, output_file):
+    merged_rows = []
+
+    for file_path in input_files:
+        # Infer cluster name from the file name
+        cluster_name = os.path.basename(file_path).replace(".tsv", "")
+
+        # Read the .tsv file into a DataFrame
+        df = pd.read_csv(file_path, sep="\t", header=None, names=["reference", "redundant"])
+
+        # Add the cluster column
+        df["cluster"] = cluster_name
+
+        # Append to the merged rows
+        merged_rows.append(df)
+
+    # Concatenate all DataFrames
+    merged_df = pd.concat(merged_rows, ignore_index=True)
+
+    # Save the merged DataFrame to the output file
+    merged_df.to_csv(output_file, sep="\t", index=False)
+
+if __name__ == "__main__":
+    input_files = sys.argv[1:-1]  # All input files passed as space-separated arguments
+    output_file = sys.argv[-1]   # The last argument is the output file
+    main(input_files, output_file)
--- workflow/scripts/pangenome_info.py
+++ workflow/scripts/pangenome_info.py
@@ -15,13 +15,15 @@ def main():
     kofams_df = pd.read_csv(kofams)
 
     # Modify inputs
-    clusters_df["genome"] = clusters_df["genome"].str.replace(".fna", "")
+    clusters_df["genome"] = clusters_df["genome"].str.replace(".fna", "", regex=False)
     clusters_df['secondary_cluster'] = 'cluster' + clusters_df['secondary_cluster'].astype(str)
 
 
+    clusters_df = clusters_df.rename(columns={"secondary_cluster": "cluster"})
+
     merged_df1 = pd.merge(
         genomes_df,
-        clusters_df[["genome", "secondary_cluster"]],
+        clusters_df[["genome", "cluster"]],
         left_on="genome",
         right_on="genome",
         how="left"
@@ -30,16 +32,10 @@ def main():
     merged_df2 = pd.merge(
         merged_df1,
         lengths_df,
-        left_on="contig",
-        right_on="contig",
-        how="left"
+        left_on="gene",
+        right_on="gene",
+        how="right"
     )
-
-    merged_df2 = merged_df2.rename(columns={"secondary_cluster": "cluster"})
-
-    print(merged_df2)
-
-    print(kofams_df)
 
     merged_df3 = pd.merge(
         merged_df2,
@@ -48,6 +44,8 @@ def main():
         right_on="gene",
         how="left"
     )
+
+    print(merged_df3)
 
     # Select the required columns and save
     final_df = merged_df3[["cluster", "genome", "gene", "length", "ko_id", "ko_e"]]
